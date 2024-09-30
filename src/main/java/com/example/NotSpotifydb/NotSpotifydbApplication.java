7ef2fceec6cdea8288c1b9dbf41f42fe9dd3f07a
George Anayo-Ezikeoha <chukwumageorge78@gmail.com>
2024-09-30 11:48:24 +0100

    commit 1.0

---
+++ src/main/java/com/example/NotSpotifydb/NotSpotifydbApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotSpotifydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotSpotifydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotSpotifydbApplication.class, args);
+	}
+
+}
